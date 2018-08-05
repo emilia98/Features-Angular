6f813fd92672dfeb903014921f1ed3857cdea034
emilia98 <emilia_nedialkova@abv.bg>
2018-08-05 23:55:28 +0300

    Updated image slider to work in both directions

--- image-slider/src/app/slider/slider.component.ts
+++ image-slider/src/app/slider/slider.component.ts
@@ -11,29 +11,32 @@ export class SliderComponent implements OnInit {
 
     constructor() {
         this.imageUrls = [
-            "http://placehold.it/350x250/e8117f/fff&amp;text=Product+Main",
-            "http://placehold.it/350x250/00ffff/000&amp;text=Product+Image+2",
-            "http://placehold.it/350x250/ff00ff/fff&amp;text=Product+Image+3",
-            "http://placehold.it/350x250/ffff00/000&amp;text=Product+Image+4",
-            "http://placehold.it/350x250/7B1C8E/fff&amp;text=Product+Image+5",
-            "http://placehold.it/350x250/9EF383/000&amp;text=Product+Image+6",
-            "http://placehold.it/350x250/D64908/fff&amp;text=Product+Image+7",
-            "http://placehold.it/350x250/E3A3A1/000&amp;text=Product+Image+8",
+            "https://cdn.lynda.com/course/438407/438407-635907149496560185-16x9.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScB7zF23Gc_f3zVjtrH3gNumXHTLHNup1T5JB8xWz7zlTb57PW",
+            "https://xenforo.com/community/media/1.188/full",
+            "https://vignette.wikia.nocookie.net/avali/images/7/7d/2-graphic.png/revision/latest?cb=20140201072016",
+            "https://i.imgur.com/vLqz2gS.png",
+            "https://s3.amazonaws.com/tinycards/image/46d8c4f31fa6ac3edf3ee4dc3f004613",
+            "http://www.godhungry.org/wp-content/uploads/2012/08/5.png",
+            "http://pngimg.com/uploads/number6/number6_PNG18572.png",
+            "https://upload.wikimedia.org/wikipedia/commons/7/75/MetroDF_Linea_7.jpg",
+            "https://michellebowden.com.au/media2/2012/12/8.png",
         ];
         this.currentImage = this.imageUrls[0];
     }
 
     slideLeft() {
-
+        this.imageUrls.unshift(this.imageUrls.pop());
+        this.currentImage = this.imageUrls[0];
     }
 
     slideRight() {
-        console.log('right');
+        // console.log('right');
 
         // console.log(this.imageUrls.p)
         this.imageUrls.push(this.imageUrls.shift());
         this.currentImage = this.imageUrls[0];
-        console.log(this.imageUrls);
+        // console.log(this.imageUrls);
     }
 
     ngOnInit() {
